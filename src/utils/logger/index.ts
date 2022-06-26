d1a75f4aa78c59bcec837979bab4cf19c97c0a25
Sam Newhouse <hello@samnewhouse.co.uk>
2022-06-26 13:16:30 +0100

    Updated dependencies, replaced winston with pino

--- src/utils/logger/index.ts
+++ src/utils/logger/index.ts
@@ -1,28 +1,8 @@
-import { createLogger, Logger, LoggerOptions, format } from 'winston';
-const { combine, timestamp, splat, printf, errors, json } = format;
+import Pino from 'pino';
 
-// Transports
-import consoleTransport from './transports/console';
-
-const logFormat =
-  process.env.NODE_ENV === 'development'
-    ? printf(({ level, message, stack, timestamp, errorCode = '' }) => {
-        let log = `${timestamp} ${level} ${errorCode}: ${message}`;
-
-        if (stack) {
-          log = `${log}\n${stack}`;
-        }
-        return log;
-      })
-    : json();
-
-const options: LoggerOptions = {
-  format: combine(errors({ stack: true }), splat(), timestamp(), logFormat),
-  level: 'info',
-  transports: [consoleTransport],
-  exitOnError: false,
-};
-
-const logger: Logger = createLogger(options);
+const logger = Pino({
+  name: 'serverless-boilerplate',
+  level: 'debug',
+});
 
 export default logger;
--- src/utils/logger/transports/console.ts
+++
@@ -1,7 +0,0 @@
-import { transports } from 'winston';
-
-const options: transports.ConsoleTransportOptions = {
-  handleExceptions: true,
-};
-
-export default new transports.Console(options);
